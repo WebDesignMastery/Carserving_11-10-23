cfeea3b9ff52f57c62507c4665ebe06e9cb0810b
Web Design Mastery <130627020+WebDesignMastery@users.noreply.github.com>
2023-10-11 03:01:14 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOptions = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOptions,
+});
+
+ScrollReveal().reveal(".header__btn", {
+  ...scrollRevealOptions,
+  delay: 500,
+});
+
+// service container
+ScrollReveal().reveal(".service__card", {
+  ...scrollRevealOptions,
+  interval: 500,
+});
+
+// price container
+ScrollReveal().reveal(".price__card", {
+  ...scrollRevealOptions,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
